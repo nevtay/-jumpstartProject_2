9c974379ab0db7aec9fe7cdcd0abd38bd189ce14
nevtay <nevintay@gmail.com>
2020-02-17 11:22:32 +0800

    modified server start text slightly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const app = require("./app");
+const app = require('./app');
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
-    console.log(`App started on http://localhost:${port}`)
+  console.log(` :) App started on http://localhost:${port}`);
 })
+;
