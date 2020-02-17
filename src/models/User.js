4472d2d32c98b82f6de297af06e79686cfe43288
nevtay <nevintay@gmail.com>
2020-02-17 17:00:08 +0800

    added schema for users and tweet-like messages called 'thoughts'

--- src/models/User.js
+++ src/models/User.js
@@ -1,34 +1,48 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const thoughtSchema = Schema({
-    id: {
-        type: String
-    },
-    content: {
-        type: String,
-        max: 140,
-    },
-    datePosted: {
-        type: Date,
-        default: Date.now
-    }
-})
+const thoughtSchema = new Schema({
+  id: {
+    type: String,
+    unique: true,
+  },
+  content: {
+    type: String,
+    max: 140,
+  },
+  datePosted: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const userSchema = Schema({
-    username: {
-        type: String,
-        required: true,
-        min: 2,
-        max: 50,
-    },
-    joinDate: {
-        type: Date,    
-        Default: Date.now
-    },
-    aboutUser: {
-        type: String,
-        max:, 500
-    },
-    thoughtsArray: [thoughtSchema]
-})
+const UserSchema = new Schema({
+  username: {
+    required: true,
+    type: String,
+    unique: true,
+    min: 3,
+    max: 50,
+  },
+  email: {
+    required: true,
+    type: String,
+    unique: true,
+  },
+  password: {
+    required: true,
+    type: String,
+    min: 5,
+  },
+  joinDate: {
+    type: Date,
+    Default: Date.now,
+  },
+  aboutUser: {
+    type: String,
+    max: 500,
+  },
+  thoughtsArray: [thoughtSchema],
+});
+
+module.exports = mongoose.model('User', UserSchema);
