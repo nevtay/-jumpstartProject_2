b5394df1ae3a49de351fa976677386382f2d39e7
nevtay <nevintay@gmail.com>
2020-02-19 11:26:38 +0800

    refactored code

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,12 +2,22 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// WHITELIST >>> CURRENT <<< IP ADDRESS
+const mongoOptions = {
+  useNewUrlParser: true, // prevent deprecation warnings
+  useUnifiedTopology: true,
+  useFindAndModify: false, // For find one and update
+  useCreateIndex: true, // for creating index with unique
+};
+
 mongoose.connect(
     process.env.DB_CONNECT,
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true, // for creating index with unique
-    },
-    () => console.log('connected to db!'),
+    mongoOptions,
 );
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error'));
+
+db.once('open', () => {
+  console.log('connected to db!');
+});
