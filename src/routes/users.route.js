125a104136e9c7c44f55dc2772f265abc94a49dc
nevtay <nevintay@gmail.com>
2020-02-19 11:25:33 +0800

    adding login and logout POST methods; added comments to identify purpose of code blocks

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+// create JWT token function
+const createJWTToken = (myId, myUserName) => {
+  const today = new Date();
+  const exp = new Date(today);
+
+  const secret = getJWTSecret();
+  exp.setDate(today.getDate() + 60);
+
+  const payload = {
+    id: myId,
+    userName: myUserName,
+    exp: parseInt(exp.getTime() / 1000),
+  };
+  const token = jwt.sign(payload, secret);
+  return token;
+};
+
+// default GET
 router.get('/', (req, res) => {
   res
       .status(200)
-      .send('Path to /user is working');
+      .send('GET /users is working');
 });
 
+// GET user by name
 router.get('/:username', async (req, res, next) => {
-  const USER_DOES_NOT_EXIST = 'Uh oh - User doesn\'t exist!';
+  const USER_DOES_NOT_EXIST = `Uh oh - user "${req.params.username}" doesn't exist!`;
   const filteredUser = String(req.params.username);
   try {
     const userExists = await User.findOne(
@@ -29,6 +50,55 @@ router.get('/:username', async (req, res, next) => {
   }
 });
 
+// POST login validation
+router.post('/login', async (req, res, next) => {
+  try {
+    const {username, password} = req.body;
 
-module.exports = router
-;
+    // check if username exists
+    const userExists = await User.findOne({username});
+    if (userExists === null) {
+      throw new Error('Invalid username');
+    }
+
+    // check if password exists
+    const validPassword = await bcrypt.compare(password, userExists.password);
+    if (!validPassword) {
+      throw new Error('Invalid password');
+    }
+
+    // create token with JWT
+    const loginToken = createJWTToken(userExists._id, userExists.username);
+    const oneDay = 24 * 60 * 60 * 1000;
+    const expiresInOneDay = new Date(Date.now() + oneDay);
+
+    res.cookie('loginToken', loginToken, {
+      expires: expiresInOneDay,
+      httpOnly: true,
+    });
+
+    res.send(token);
+  } catch (error) {
+    console.log(error);
+    if (error.message === 'Invalid username' || error.message === 'Invalid password') {
+      error.statusCode = 400;
+    }
+    next(error);
+  }
+});
+
+// POST logout
+// router.post('/logout', (req, res) => {
+//   res.clearCookie('token').send('Log out successful');
+// });
+
+module.exports = router;
+
+/*
+test credentials
+{
+	"username": "myusername",
+	"email": "myusername@gmail.com",
+	"password": "mypassword"
+}
+*/
