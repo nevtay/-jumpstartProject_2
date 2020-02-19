93ea845ec49ceee672df473e1862030916b4ed70
nevtay <nevintay@gmail.com>
2020-02-19 17:38:57 +0800

    updated to reflect prettier rules

--- src/app.js
+++ src/app.js
@@ -9,20 +9,17 @@ app.use(express.json());
 app.use('/users', userRoute);
 app.use('/register', registerRoute);
 app.get('/', (req, res) => {
-  res
-      .status(200)
-      .json({
-        '0': 'GET /users',
-        '1': 'GET /users/:username',
-        '2': 'GET /users/:username',
-        '3': 'POST /users/login',
-        '4': 'PATCH /users/:username',
-        '5': 'DELETE /users/:username',
-        '6': 'POST /users/:username/tweets',
-        '7': 'DELETE /users/:username/tweets',
-      });
+  res.status(200).json({
+    '0': 'GET /users',
+    '1': 'GET /users/:username',
+    '2': 'GET /users/:username',
+    '3': 'POST /users/login',
+    '4': 'PATCH /users/:username',
+    '5': 'DELETE /users/:username',
+    '6': 'POST /users/:username/tweets',
+    '7': 'DELETE /users/:username/tweets'
+  });
 });
-
 
 app.use(middlewares.defaultErrorHandler);
 
