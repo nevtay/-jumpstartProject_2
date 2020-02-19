6490b26e9b036280afc2367c99a65fce7b2181ae
nevtay <nevintay@gmail.com>
2020-02-19 15:51:30 +0800

    initial commit

---
+++ src/config/retrieveJWTSecret.js
@@ -0,0 +1,9 @@
+const jwtKeySecret = () => {
+    const secretKeyIs = process.env.JWT_KEY_SECRET
+    if (!secretKeyIs) {
+        throw new Error('No token; bad secret');
+    }
+    return secretKeyIs;
+}
+
+module.exports = { jwtKeySecret }
