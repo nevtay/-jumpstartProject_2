4a6d3ce4ebe4e1387d94d39b432ea69e89341b86
nevtay <nevintay@gmail.com>
2020-02-19 15:50:17 +0800

    refactored db files

--- src/utils/db.js
+++ src/utils/db.js
@@ -17,7 +17,6 @@ mongoose.connect(
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error'));
-
 db.once('open', () => {
   console.log('connected to db!');
 });
