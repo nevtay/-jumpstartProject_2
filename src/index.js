c32edc467c64a03adb30ca589d074634987bc055
nevtay <nevintay@gmail.com>
2020-02-21 18:19:48 +0800

    latest changes

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = 3000;
 
 const server = app.listen(process.env.PORT || PORT, () => {
-  console.log(` :) App started on http://localhost:${port}`);
+  console.log(` :) App started on http://localhost:${PORT}`);
 });
