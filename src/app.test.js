9e45b728ff33ef818df8344f34ef207a9be9883f
nevtay <nevintay@gmail.com>
2020-02-24 09:51:36 +0800

    updated test to reflect new objectives

--- src/app.test.js
+++ src/app.test.js
@@ -10,12 +10,12 @@ describe('/', () => {
     expect(response).toEqual({
       '0': 'GET /users',
       '1': 'GET /users/:username',
-      '2': 'GET /users/:username',
       '3': 'POST /users/login',
       '4': 'PATCH /users/:username',
-      '5': 'DELETE /users/:username',
-      '6': 'POST /users/:username/tweets',
+      '5': 'PATCH /users/',
+      '6': 'POST /users/posthough',
       '7': 'DELETE /users/:username/tweets',
+      '8': 'DELETE /users',
     });
     done();
   });
